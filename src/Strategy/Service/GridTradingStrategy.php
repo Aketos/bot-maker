28520f7fdcc4105868d5923f775bce4d357151c8
Aketos <lagardemathieu@hotmail.fr>
2020-02-02 23:03:54 +0100

    initialize first strategy and add tags

---
+++ src/Strategy/Service/GridTradingStrategy.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BotMaker\Strategy\Service;
+
+use BotMaker\Strategy\StrategyInterface;
+
+class GridTradingStrategy implements StrategyInterface
+{
+    public function initialize(): bool
+    {
+        // TODO: Implement initialize() method.
+    }
+
+    public function process()
+    {
+        // TODO: Implement process() method.
+    }
+
+    public function isReady(): bool
+    {
+        // TODO: Implement isReady() method.
+    }
+
+    public function isActive(): bool
+    {
+        // TODO: Implement isActive() method.
+    }
+}

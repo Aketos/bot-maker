8f8135fe6980f01e596ded962ffa3e34d454e366
Aketos <lagardemathieu@hotmail.fr>
2020-02-03 23:34:25 +0100

    update gitignore + define grid-trading config to set

--- src/StrategyBundle/Service/GridTradingStrategy.php
+++ src/StrategyBundle/Service/GridTradingStrategy.php
@@ -5,6 +5,18 @@ namespace BotMaker\StrategyBundle\Service;
 class GridTradingStrategy extends BaseStrategy
 {
 
+    /* Configuration to define
+     * -----------------------------------------------------------------------
+     *  pairAToken: Token to trade (ex: VET)
+     *  pairABalance: Amount of token to trade (ex: 1000)
+     *  pairBToken: Token used as market (includes fiat - USD)
+     *  pairBBalance: Max price to set on grid
+     *  minPriceExpected: Min value of the grid
+     *  maxPriceExpected: Max value of the grid
+     *  orderSize: Number of tokens to trade per order
+     *  grids: Number of grids
+     */
+
     public function initialize(): bool
     {
         // TODO: Implement initialize() method.
